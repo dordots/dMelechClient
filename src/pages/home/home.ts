3514dd63740f082fc7b8d2f5af2347382513073e
123 <123>
2018-05-30 21:19:02 +0300

    Add map to content page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import * as GoogleMaps from "google-maps"
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,33 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+
   constructor(public navCtrl: NavController) {
 
   }
+
+  ionViewDidLoad(){
+    this.loadMap();
+  }
+ 
+  loadMap(){
+    GoogleMaps.load(google => {
+        
+      let latLng = new google.maps.LatLng(31.778018, 35.235324);
+  
+      let mapOptions: google.maps.MapOptions = {
+        center: latLng,
+        zoom: 16,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        disableDefaultUI: true
+      }
+  
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+  
+    })
+  }
+
 
 }
