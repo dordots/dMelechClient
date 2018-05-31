c52373dd5faef79907ee4952f0e4945538d86f46
Dor Saban <dordots@gmail.com>
2018-06-01 00:01:54 +0300

    Change text to hebrew. change app name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,18 +19,15 @@ export class MyApp {
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
 
-    // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'ראשי', component: HomePage },
+      { title: 'רשימה', component: ListPage }
     ]; 
 
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
