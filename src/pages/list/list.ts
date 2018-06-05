6518beae73fa6cfb848d45c62819f48e0228bf1d
Dor Saban <dordots@gmail.com>
2018-06-05 19:24:21 +0300

    synagogue card first template

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,37 +1,25 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, DateTime } from 'ionic-angular';
+import { Time } from '@angular/common';
 
 @Component({
   selector: 'page-list',
   templateUrl: 'list.html'
 })
 export class ListPage {
-  selectedItem: any;
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  synagogueName: string;
+  soonMinyanTime: string;
+  soonMinyanType: string;
+  lastUpdatedTime: string;
+  distance: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
-
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
-
-    this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
-  }
-
-  itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push(ListPage, {
-      item: item
-    });
-  }
+    let TfilaTime = new Date('June 05, 2018 19:24:00');
+  
+    this.synagogueName = 'אוהל משה';
+    this.soonMinyanTime = TfilaTime.getHours() + ':' +TfilaTime.getMinutes();
+    this.soonMinyanType = 'ערבית';
+    this.lastUpdatedTime = 'לפני 2 ימים';
+    this.distance = '300 מטרים';
+   }
 }
