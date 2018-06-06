5e089939b86d4ac48d5475d460a5490095361a34
Dor Saban <dordots@gmail.com>
2018-06-06 20:20:30 +0300

    order synagogue list page

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,25 +1,42 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, DateTime } from 'ionic-angular';
-import { Time } from '@angular/common';
+import { NavController, NavParams } from 'ionic-angular';
+
+enum TfilaType {
+  MORNING = 'שחרית',
+  AFTERNOOT = 'מנחה',
+  EVENING = 'ערבית'
+};
 
 @Component({
   selector: 'page-list',
   templateUrl: 'list.html'
 })
+
 export class ListPage {
-  synagogueName: string;
-  soonMinyanTime: string;
-  soonMinyanType: string;
-  lastUpdatedTime: string;
-  distanceString: string;
+  SynagogueList: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    let TfilaTime = new Date('June 05, 2018 19:24:00');
-  
-    this.synagogueName = 'אוהל משה';
-    this.soonMinyanTime = TfilaTime.getHours() + ':' +TfilaTime.getMinutes();
-    this.soonMinyanType = 'ערבית';
-    this.lastUpdatedTime = 'לפני 2 ימים';
-    this.distanceString = '300 מטרים';
+   
+    this.SynagogueList = [{
+      synagogueName: 'אוהל משה',
+      soonMinyanType: TfilaType.EVENING,
+      soonMinyanTime: '19:30',
+      lastUpdatedTime: 'לפני 2 ימים',
+      distanceString: '300 מטרים'
+    },
+    {
+      synagogueName: 'בית יעקב',
+      soonMinyanType: TfilaType.EVENING,
+      soonMinyanTime: '19:20',
+      lastUpdatedTime: 'לפני 2 ימים',
+      distanceString: '403 מטרים'
+    },
+    {
+      synagogueName: 'אוהל שרה',
+      soonMinyanType: TfilaType.EVENING,
+      soonMinyanTime: '19:24',
+      lastUpdatedTime: 'לפני 8 ימים',
+      distanceString: '200 מטרים'
+    }];
    }
 }
