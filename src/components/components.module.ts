82dba7fca25f429043d716706129abd456236a4b
yehudit <ym221293@gmail.com>
2018-06-06 22:55:32 +0300

    added list of synagogue to list-view

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
+import { IonicModule } from 'ionic-angular';
 import { MapComponent } from './map/map';
 import { ListViewComponent } from './list-view/list-view';
+
 @NgModule({
 	declarations: [MapComponent,
     ListViewComponent],
-	imports: [],
+	imports: [IonicModule],
 	exports: [MapComponent,
     ListViewComponent]
 })
--- src/components/list-view/list-view.ts
+++ src/components/list-view/list-view.ts
@@ -1,22 +1,46 @@
 import { Component } from '@angular/core';
+import { IonicModule } from 'ionic-angular';
 
-/**
- * Generated class for the ListViewComponent component.
- *
- * See https://angular.io/api/core/Component for more info on Angular
- * Components.
- */
+enum TfilaType {
+  MORNING = 'שחרית',
+  AFTERNOOT = 'מנחה',
+  EVENING = 'ערבית'
+};
+
 @Component({
   selector: 'list-view',
   templateUrl: 'list-view.html'
 })
+
 export class ListViewComponent {
 
+  SynagogueList: any;
   text: string;
 
   constructor() {
     console.log('Hello ListViewComponent Component');
-    this.text = 'Hello World';
+    this.text = "hello world!!!"
+    this.SynagogueList = [{
+      synagogueName: 'אוהל משה',
+      soonMinyanType: TfilaType.EVENING,
+      soonMinyanTime: '19:30',
+      lastUpdatedTime: 'לפני 2 ימים',
+      distanceString: '300 מטרים'
+    },
+    {
+      synagogueName: 'בית יעקב',
+      soonMinyanType: TfilaType.EVENING,
+      soonMinyanTime: '19:20',
+      lastUpdatedTime: 'לפני 2 ימים',
+      distanceString: '403 מטרים'
+    },
+    {
+      synagogueName: 'אוהל שרה',
+      soonMinyanType: TfilaType.EVENING,
+      soonMinyanTime: '19:24',
+      lastUpdatedTime: 'לפני 8 ימים',
+      distanceString: '200 מטרים'
+    }];
   }
 
 }
