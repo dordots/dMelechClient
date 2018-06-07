384b3ec08221ed49080293f4bcdb8e3d40fa7fa7
davidbulka <davidbulka@gmail.com>
2018-06-07 16:40:25 +0300

    adding interfaces for coordinates, event and location

--- src/models/Location.ts
+++ src/models/Location.ts
@@ -1,5 +1,14 @@
+import { IEntity } from './Entity';
+import { ICoordinates } from './Coordinates';
 
-export interface ILocation {
-    lat: number;
-    lng: number;
+export interface ILocation extends IEntity {
+
+    /** @prop The name of the location */
+    name: string;
+
+    /** @prop The coordinates of the location */
+    coordinates: ICoordinates;
+
+    /** @prop Is the location is active (not in shiputzim for example) */
+    isActive: boolean;
 }
