702ecfcb0b90918eb76fb63d17009820db01a0b6
Dor Saban <dordots@gmail.com>
2018-06-08 14:01:50 +0300

    enter date manipulates

--- src/components/list-view/list-view.ts
+++ src/components/list-view/list-view.ts
@@ -13,19 +13,63 @@ enum TfilaType {
 })
 
 export class ListViewComponent {
-
   SynagogueList: any;
-  text: string;
 
   constructor() {
-    console.log('Hello ListViewComponent Component');
-    this.text = "hello world!!!"
+    var oneDay = 86400000; 
+    let dateOfMinyan = new Date('December 17, 1995 03:24:00');
+    let dateTimeNow = new Date('June 08, 2018 03:24:00');
+
+    let diffDays = Math.round(Math.abs((dateOfMinyan.getTime() - dateTimeNow.getTime())/(oneDay)));
+    
     this.SynagogueList = [{
       synagogueName: 'אוהל משה',
       soonMinyanType: TfilaType.EVENING,
       soonMinyanTime: '19:30',
       lastUpdatedTime: 'לפני 2 ימים',
       distanceString: '300 מטרים'
+    },
+    {
+      synagogueName: 'בית יעקב',
+      soonMinyanType: TfilaType.EVENING,
+      soonMinyanTime: '19:20',
+      lastUpdatedTime: 'לפני 2 ימים',
+      distanceString: '403 מטרים'
+    },
+    {
+      synagogueName: 'אוהל שרה',
+      soonMinyanType: TfilaType.EVENING,
+      soonMinyanTime: '19:24',
+      lastUpdatedTime: 'לפני 8 ימים',
+      distanceString: '200 מטרים'
+    },
+    {
+      synagogueName: 'בית יעקב',
+      soonMinyanType: TfilaType.EVENING,
+      soonMinyanTime: '19:20',
+      lastUpdatedTime: 'לפני 2 ימים',
+      distanceString: '403 מטרים'
+    },
+    {
+      synagogueName: 'אוהל שרה',
+      soonMinyanType: TfilaType.EVENING,
+      soonMinyanTime: '19:24',
+      lastUpdatedTime: 'לפני 8 ימים',
+      distanceString: '200 מטרים'
+    },
+    {
+      synagogueName: 'בית יעקב',
+      soonMinyanType: TfilaType.EVENING,
+      soonMinyanTime: '19:20',
+      lastUpdatedTime: 'לפני 2 ימים',
+      distanceString: '403 מטרים'
+    },
+    {
+      synagogueName: 'אוהל שרה',
+      soonMinyanType: TfilaType.EVENING,
+      soonMinyanTime: '19:24',
+      lastUpdatedTime: 'לפני 8 ימים',
+      distanceString: '200 מטרים'
     },
     {
       synagogueName: 'בית יעקב',
