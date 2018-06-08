46c6babb718972d83f683423342a3dac4aa346dd
davidbulka <davidbulka@gmail.com>
2018-06-08 16:14:41 +0300

    remove prev commit

--- src/providers/location-track/location-track.ts
+++ src/providers/location-track/location-track.ts
@@ -1,23 +1,20 @@
-import { ICoordinates } from './../../models/Coordinates';
-import { Injectable } from '@angular/core';
-import { Geolocation } from '@ionic-native/geolocation';
+import { ICoordinates } from "./../../models/Coordinates";
+import { Injectable } from "@angular/core";
+import { Geolocation } from "@ionic-native/geolocation";
 
 /*
   Provides info about the location of the current device.
 */
 @Injectable()
 export class LocationTrackProvider {
-
-  constructor(public geolocation: Geolocation) {
-
-  }
+  constructor(public geolocation: Geolocation) {}
 
   getCurrentPosition(): Promise<ICoordinates> {
     return this.geolocation.getCurrentPosition().then(position => {
       let coordinates: ICoordinates = {
         lat: position.coords.latitude,
         lng: position.coords.longitude
-      }
+      };
       return coordinates;
     });
   }
