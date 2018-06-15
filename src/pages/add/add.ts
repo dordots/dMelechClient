1c21387b9c9f5df3df9ae01c55e9e4a67437ee19
Dor Saban <dordots@gmail.com>
2018-06-15 14:22:23 +0300

    add tabs to add page - need to change it to icons ?

--- src/pages/add/add.ts
+++ src/pages/add/add.ts
@@ -2,10 +2,8 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 /**
- * Generated class for the AddPage page.
+ * add new synagogue/ lesson / mikve
  *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
  */
 
 @IonicPage()
