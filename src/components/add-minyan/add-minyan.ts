8d10f627fae1f4765c6aa46fa0d5e9febedbf88f
Yehudit <ym221293@gmail.com>
2018-07-09 21:53:19 +0300

    added dismiss function to add_minyan comp.

--- src/components/add-minyan/add-minyan.ts
+++ src/components/add-minyan/add-minyan.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicModule } from 'ionic-angular';
+import { IonicModule, ViewController } from 'ionic-angular';
 
 @Component({
   selector: 'add-minyan',
@@ -10,14 +10,24 @@ export class AddMinyanComponent {
 
   private daysInWeek : any;
   private keys : any;
+  private viewCtrl:ViewController;
 
-  constructor() {
+  constructor(public _viewCtrl: ViewController ) {
     this.daysInWeek  =  { 'א': "light", 'ב': "light",'ג': "light",'ד': "light",'ה': "light",'ו': "light",'ז': "light",}
     this.keys = Object.keys(this.daysInWeek);
+    this.viewCtrl = _viewCtrl;
   }
 
   toggleDay(key : string) {
     this.daysInWeek[key]= (this.daysInWeek[key] == "light") ? "primary" : "light" ;
+  }
+
+  add() {
+    this.viewCtrl.dismiss();
+  }
+
+  cancel() {
+    this.viewCtrl.dismiss();
   }
 
 }
